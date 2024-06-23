58f89720f80bb16946a127ee6fe2d870da96e932
qaswarsarfraz187 <numl-f23-26939@numls.edu.pk>
2024-06-23 07:03:10 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-// main.js
+// Define the byeSpeaker and helloSpeaker objects
+var byeSpeaker = {
+    speak: function(name) {
+        console.log("Goodbye " + name);
+    }
+};
 
+var helloSpeaker = {
+    speak: function(name) {
+        console.log("Hello " + name);
+    }
+};
+
+// main.js
 (function () {
-    var names = ["adam", "amy", "Jon", "paro", "nakual", "joseph", "henry", "qaswar", "pablo", "yujin];
+    var names = ["adam", "amy", "Jon", "paro", "nakual", "joseph", "henry", "qaswar", "pablo", "yujin"];
 
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
